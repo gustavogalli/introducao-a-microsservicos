7b27a94c078ee18e20f230f81a85f2488b0e475d
Gustavo Galli <75743915+gustavogalli@users.noreply.github.com>
2024-02-10 15:26:02 -0300

    Delete user-api directory

--- user-api/src/main/java/com/example/userapi/service/exceptions/ObjectNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.userapi.service.exceptions;
-
-public class ObjectNotFoundException extends RuntimeException {
-
-    public ObjectNotFoundException(String message) {
-        super(message);
-    }
-}
