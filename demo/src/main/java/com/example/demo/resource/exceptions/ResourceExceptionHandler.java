62c71c0da2b6fc14985b88b779ed7cd108bd8ab9
gustavogalli <gustavosgalli@gmail.com>
2024-02-10 20:31:55 -0300

    Adding Eureka server and clients

--- demo/src/main/java/com/example/demo/resource/exceptions/ResourceExceptionHandler.java
+++ demo/src/main/java/com/example/demo/resource/exceptions/ResourceExceptionHandler.java
@@ -1,12 +1,12 @@
 package com.example.demo.resource.exceptions;
 
 import com.example.demo.service.exceptions.ObjectNotFoundException;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 
 @ControllerAdvice
