d82d40796ebfc9890a6e16256def63d132a6ff87
gustavogalli <gustavosgalli@gmail.com>
2024-02-10 21:51:19 -0300

    Adding log into UserServiceImpl and removing local URL in order to have load balancing

--- demo/src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -6,19 +6,24 @@ import com.example.demo.service.UserService;
 import com.example.demo.service.exceptions.ObjectNotFoundException;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository repository;
+    private final Environment env;
 
     @Override
     public User findById(Long id) {
+        log.info("PAYROLL SERVICE ::: Get request on " + env.getProperty("local.server.port") + " port");
         return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado!"));
     }
 
